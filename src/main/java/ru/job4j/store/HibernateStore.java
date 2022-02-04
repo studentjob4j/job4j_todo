4d555b74d285c66eef390c237bbcab5badbb4ef6
Evgenii <job4jstudy@yandex.ru>
2022-02-04 17:31:26 +1000

    Лямбды и шаблон wrapper

--- src/main/java/ru/job4j/store/HibernateStore.java
+++ src/main/java/ru/job4j/store/HibernateStore.java
@@ -2,14 +2,15 @@ package ru.job4j.store;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.job4j.model.Item;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 /**
  * @author Shegai Evgenii
@@ -17,7 +18,7 @@ import java.util.List;
  * @version 1.0
  */
 
-public class HibernateStore implements Store {
+public class HibernateStore implements Store, AutoCloseable {
 
     private static final Logger LOG = LoggerFactory.getLogger(HibernateStore.class.getName());
 
@@ -29,6 +30,11 @@ public class HibernateStore implements Store {
             .buildMetadata()
             .buildSessionFactory();
 
+    @Override
+    public void close() throws Exception {
+        StandardServiceRegistryBuilder.destroy(registry);
+    }
+
     private static final class HibernateStoreHolder {
         private static final Store INSTANCE = new HibernateStore();
     }
@@ -39,55 +45,40 @@ public class HibernateStore implements Store {
 
     @Override
     public List<Item> findAllItems() {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        List list = new ArrayList<>();
-        try {
-            list = session.createQuery("from ru.job4j.model.Item").getResultList();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-          LOG.error(e.getMessage(), e);
-        } finally {
-            session.close();
-        }
-        return list;
+        return this.tx(session -> session.createQuery("from ru.job4j.model.Item").getResultList());
     }
 
     @Override
     public Item createItem(String description) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
         Item item = new Item();
-        try {
-            item.setDescription(description);
-            session.save(item);
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            LOG.error(e.getMessage(), e);
-        } finally {
-            session.close();
-        }
+        item.setDescription(description);
+        this.tx(session -> session.save(item));
         return item;
     }
 
     @Override
     public Item updateItem(int id) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
+        return this.tx(session -> {
+           Item item = session.get(Item.class, id);
+           item.setDone(!item.isDone());
+           return item;
+        });
+    }
+
+
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = sessionFactory.openSession();
+        final Transaction tx = session.beginTransaction();
         try {
-            Item item = session.get(Item.class, id);
-            if (item == null) {
-                throw new IllegalStateException("Could not find a record in DB");
-            }
-            item.setDone(!item.isDone());
-            session.update(item);
-            session.getTransaction().commit();
-            return item;
-        } catch (Exception e) {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
             LOG.error(e.getMessage(), e);
+            throw e;
         } finally {
             session.close();
         }
-        return null;
     }
 }
